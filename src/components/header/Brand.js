42d1e42dbcffe6b82b787203936c5f1c0ac37023
FranBruna <franciscojosebruna@gmail.com>
2023-11-28 20:55:49 -0300

    agregando estilos

--- src/components/header/Brand.js
+++ src/components/header/Brand.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Brand = () => {
   return (
-    <Link to="/">
+    <Link to="">
       <img src={brand} alt="Mia Tutina" title="Mia Tutina"></img>
     </Link>
   );
